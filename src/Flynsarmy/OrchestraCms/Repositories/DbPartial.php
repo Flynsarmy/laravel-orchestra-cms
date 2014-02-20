debf2ff1ad334561c49b5a12b9b8263e29a9e9b1
flynsarmy <flynsarmy@gmail.com>
2014-02-21 00:43:58 +1000

    Extend correct interface

--- src/Flynsarmy/OrchestraCms/Repositories/DbPartial.php
+++ src/Flynsarmy/OrchestraCms/Repositories/DbPartial.php
@@ -1,7 +1,7 @@
 <?php namespace Flynsarmy\OrchestraCms\Repositories;
 
 use Flynsarmy\OrchestraCms\Repositories\DbContent;
-use Flynsarmy\OrchestraCms\Repositories\Interfaces\Page as PartialRepositoryInterface;
+use Flynsarmy\OrchestraCms\Repositories\Interfaces\Partial as PartialRepositoryInterface;
 
 class DbPartial extends DbContent implements PartialRepositoryInterface {
 
